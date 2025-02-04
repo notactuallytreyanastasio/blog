829012722a0ff4d7f8bc1842e2c7461d19698508
Bobby Grayson <53058768+notactuallytreyanastasio@users.noreply.github.com>
2025-02-03 20:55:47 -0500

    working channels, python client creating messages

---
+++ lib/blog_web/channels/chat_channel.ex
@@ -0,0 +1,68 @@
+defmodule BlogWeb.ChatChannel do
+  use Phoenix.Channel
+  require Logger
+
+  @max_body_length 250
+  @max_handle_length 16
+  @table_name :chat_messages
+
+  def join("chat:lobby", _params, socket) do
+    # Create ETS table if it doesn't exist
+    :ets.new(@table_name, [:set, :public, :named_table, read_concurrency: true])
+    {:ok, socket}
+  rescue
+    ArgumentError ->
+      # Table already exists
+      {:ok, socket}
+  end
+
+  def handle_in("new_message", %{"body" => body, "user" => user, "reply_to" => reply_to}, socket) do
+    with {:ok, body} <- validate_body(body),
+         {:ok, user} <- validate_user(user),
+         {:ok, reply_to} <- validate_reply_to(reply_to) do
+
+      message = %{
+        id: generate_sha(),
+        body: body,
+        user: user,
+        reply_to: reply_to,
+        created_at: DateTime.utc_now()
+      }
+
+      :ets.insert(@table_name, {message.id, message})
+      broadcast!(socket, "new_message", message)
+
+      {:reply, {:ok, message}, socket}
+    else
+      {:error, reason} ->
+        {:reply, {:error, %{reason: reason}}, socket}
+    end
+  end
+
+  defp validate_body(body) when is_binary(body) do
+    if String.length(body) <= @max_body_length do
+      {:ok, body}
+    else
+      {:error, "Body exceeds maximum length of #{@max_body_length} characters"}
+    end
+  end
+  defp validate_body(_), do: {:error, "Body must be a string"}
+
+  defp validate_user(user) when is_binary(user) do
+    if String.length(user) <= @max_handle_length do
+      {:ok, user}
+    else
+      {:error, "User handle exceeds maximum length of #{@max_handle_length} characters"}
+    end
+  end
+  defp validate_user(_), do: {:error, "User handle must be a string"}
+
+  defp validate_reply_to(nil), do: {:ok, nil}
+  defp validate_reply_to(reply_to) when is_binary(reply_to), do: {:ok, reply_to}
+  defp validate_reply_to(_), do: {:error, "Reply_to must be nil or a string"}
+
+  defp generate_sha do
+    :crypto.strong_rand_bytes(20)
+    |> Base.encode16(case: :lower)
+  end
+end
---
+++ lib/blog_web/channels/user_socket.ex
@@ -0,0 +1,13 @@
+defmodule BlogWeb.UserSocket do
+  use Phoenix.Socket
+
+  channel "chat:*", BlogWeb.ChatChannel
+
+  @impl true
+  def connect(_params, socket, _connect_info) do
+    {:ok, socket}
+  end
+
+  @impl true
+  def id(_socket), do: nil
+end
