bf4588c731fd9c091a6eabc37e10bd3135f3bdfc
Bobby Grayson <53058768+notactuallytreyanastasio@users.noreply.github.com>
2025-02-10 20:44:24 -0500

    bluesky hose is successfully detecting muenster tweet feed

---
+++ lib/bluesky_hose.ex
@@ -0,0 +1,40 @@
+defmodule BlueskyHose do
+  use WebSockex
+  require Logger
+
+  def start_link(opts \\ []) do
+    WebSockex.start_link("wss://bsky-relay.c.theo.io/subscribe?wantedCollections=app.bsky.feed.post", __MODULE__, :fake_state, opts)
+  end
+
+  def handle_connect(_conn, state) do
+    Logger.info("Connected!")
+    IO.puts("#{DateTime.utc_now}")
+    {:ok, 0}
+  end
+
+  def handle_frame({:text, msg}, state) do
+    msg = Jason.decode!(msg)
+    case msg do
+      %{"commit" => %{"record" => %{"text" => skeet}}} = msg ->
+
+        case String.contains?(skeet, "muenster") do
+          true -> 
+            IO.puts("Got cheese skeet\n\n\n\n#{skeet}")
+            :pubsub_broadcast
+          false -> :do_nothing
+        end
+      _ -> 
+        nil
+    end
+    {:ok, state + 1}
+  end
+
+  def handle_disconnect(%{reason: {:local, reason}}, state) do
+    Logger.info("Local close with reason: #{inspect reason}")
+    {:ok, state}
+  end
+  
+  def handle_disconnect(disconnect_map, state) do
+    super(disconnect_map, state)
+  end
+end
