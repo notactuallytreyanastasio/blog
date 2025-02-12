60166a489234b39e7709d0d561494a58e5fd815d
Bobby Grayson <53058768+notactuallytreyanastasio@users.noreply.github.com>
2025-02-12 00:28:54 -0500

    fix sorting

--- lib/blog/content.ex
+++ lib/blog/content.ex
@@ -31,19 +31,28 @@ defmodule Blog.Content do
   end
 
   def categorize_posts(posts) do
-    Enum.reduce(posts, %{tech: [], non_tech: []}, fn post, acc ->
-      has_tech_tag = post.tags
-                     |> Enum.map(& &1.name)
-                     |> Enum.map(&String.downcase/1)
-                     |> Enum.any?(fn tag ->
-                       Enum.any?(@tech_tags, &(String.contains?(tag, &1)))
-                     end)
+    data = %{tech: tech_list, non_tech: non_tech_list} =
+      Enum.reduce(posts, %{tech: [], non_tech: []}, fn post, acc ->
+        has_tech_tag = post.tags
+                      |> Enum.map(& &1.name)
+                      |> Enum.map(&String.downcase/1)
+                      |> Enum.any?(fn tag ->
+                        Enum.any?(@tech_tags, &(String.contains?(tag, &1)))
+                      end)
 
-      if has_tech_tag do
-        Map.update!(acc, :tech, fn posts -> [post | posts] end)
-      else
-        Map.update!(acc, :non_tech, fn posts -> [post | posts] end)
-      end
-    end)
+        if has_tech_tag do
+          Map.update!(acc, :tech, fn posts -> [post | posts] end)
+        else
+          Map.update!(acc, :non_tech, fn posts -> [post | posts] end)
+        end
+      end)
+      |> IO.inspect()
+
+    sorted_tech = Enum.sort_by(tech_list, &(&1.written_on), {:desc, Date})
+    sorted_non_tech = Enum.sort_by(non_tech_list, &(&1.written_on), {:desc, Date})
+
+    data
+    |> Map.put(:tech, sorted_tech)
+    |> Map.put(:non_tech, sorted_non_tech)
   end
 end
