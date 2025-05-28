de13ece856e7b92e1e93f6c51b22c4b45bff6ef4
Bobby Grayson <53058768+notactuallytreyanastasio@users.noreply.github.com>
2025-05-27 22:23:10 -0400

    wip

--- lib/blog_web/live/components/section_component.ex
+++ lib/blog_web/live/components/section_component.ex
@@ -1,13 +1,13 @@
 defmodule BlogWeb.SectionComponent do
   use BlogWeb, :live_component
-  alias EarmarkParser
+  alias MDEx
 
   # Props: id, title, content
   def render(assigns) do
     ~H"""
     <section id={@id} class="section-component">
       <h3><%= @title %></h3>
-      <div><%= @content |> MDEx.to_html! |> Phoenix.HTML.raw() %></div>
+      <div><%= @content |> MDEx.to_html!([]) |> Phoenix.HTML.raw() %></div>
     </section>
     """
   end
--- lib/blog_web/live/nathan_live.ex
+++ lib/blog_web/live/nathan_live.ex
@@ -2,19 +2,16 @@ defmodule BlogWeb.NathanLive do
   use BlogWeb, :live_view
 
   @impl true
-  def render(assigns) do
-    ~H"""
-    <.live_component module={BlogWeb.NathanLive.RenderPage} id="nathan-page" />
-    """
+  def mount(_params, _session, socket) do
+    # Disable the default application layout.
+    # The template lib/blog_web/live/nathan_live.html.heex will be the entire response.
+    {:ok, assign(socket, :layout, false)}
   end
 
-  defmodule RenderPage do
-    use Phoenix.LiveComponent
-
-    def render(assigns) do
-      ~H""" 
-      <%= Phoenix.LiveView.Static.render_existing(BlogWeb.NathanLive.RenderPage, "nathan_live.html", assigns) %>
-      """
-    end
-  end
+  # By convention, Phoenix LiveView will now look for and render
+  # lib/blog_web/live/nathan_live.html.heex because:
+  # 1. We are in the BlogWeb.NathanLive module.
+  # 2. We have set :layout to false.
+  # 3. An explicit render/1 function is not defined.
+  # No need for the RenderPage component or the previous render/1 function.
 end
