e6a7fef35a33e60c34ae1a0ed1b5c1820acd7454
Bobby Grayson <53058768+notactuallytreyanastasio@users.noreply.github.com>
2025-06-16 23:16:22 -0400

    Match nathan_for_us buildpack config exactly - remove node_name and erlang_cookie

--- config/runtime.exs
+++ config/runtime.exs
@@ -56,6 +56,10 @@ if config_env() == :prod do
       "https://salmon-unselfish-aphid.gigalixirapp.com"
     ],
     http: [
+      # Enable IPv6 and bind on all interfaces.
+      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
+      # See the documentation on https://hexdocs.pm/bandit/Bandit.html#t:options/0
+      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
       port: port
     ],
